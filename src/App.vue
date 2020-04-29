1aaf2a5c9813186d4fc337386351395298fa3e47
Nick <nikheal25@gmail.com>
2020-04-29 22:48:25 +1000

    New function

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,21 @@
   <div id="app">
     <Header />
     <Todos v-bind:todos="todoList" v-on:del-todo="deleteToDo" />
+    <AddTodo v-on:add-todo="addNewTodo" />
   </div>
 </template>
 
 <script>
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
+
 export default {
   name: "App",
   components: {
     Todos,
-    Header
+    Header,
+    AddTodo,
   },
   data() {
     return {
@@ -20,19 +24,24 @@ export default {
         {
           id: 1,
           title: "wash cloths",
-          completed: false
+          completed: false,
         },
         { id: 2, title: "trim hair", completed: false },
-        { id: 3, title: "do homework", completed: true }
-      ]
+        { id: 3, title: "do homework", completed: true },
+      ],
     };
   },
   methods: {
     deleteToDo(id) {
-      this.todoList = this.todoList.filter(todo => todo.id !== id);
+      this.todoList = this.todoList.filter((todo) => todo.id !== id);
       console.log(id);
-    }
-  }
+    },
+    addNewTodo(item) {
+      console.log("hello");
+
+      this.todoList = [...this.todoList, item];
+    },
+  },
 };
 </script>
 
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <form @submit="addToDoList">
+      <input
+        type="text"
+        v-model="newTask"
+        name="newTask"
+        placeholder="New Todo..."
+      />
+      <button type="submit" value="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { v4 as uuid } from "uuid";
+
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      newTask: "",
+    };
+  },
+  methods: {
+    addToDoList(e) {
+      e.preventDefault();
+      console.log("Hello");
+
+      const newTodo = {
+        id: uuid(),
+        title: this.newTask,
+        completed: false,
+      };
+      //   //send the object
+      console.log(newTodo);
+
+      this.$emit("add-todo", newTodo);
+
+      this.newTask = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+}
+
+button {
+  display: inline-block;
+  border: none;
+  background: black;
+  color: aliceblue;
+  padding: 10px;
+}
+</style>
